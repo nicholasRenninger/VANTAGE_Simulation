d54d3aa7a21d1db9fa81a5a1907fb514af38495b
Nicholas Renninger <nire1188@colorado.edu>
2018-11-11 03:15:50 -0700

    Added full animation capabilities and updated model geometry

--- runSimulation_main.py
+++ runSimulation_main.py
@@ -62,8 +62,12 @@ def simulationSetup(settingsFile, doc):
     # velocities and the maximum z-dist of the simulation
     maxRng = settings['MAX_RANGE']
     ZVels = settings['CUBESAT_LIN_VELS']['v_z']
-    maxTime = c4d.BaseTime(maxRng / min(ZVels))
-    doc.SetMaxTime(maxTime)
+    maxTimeInS = maxRng / min(ZVels)
+    maxTimeInC4DBaseTime = c4d.BaseTime(maxTimeInS)
+    doc.SetMaxTime(maxTimeInC4DBaseTime)
+
+    # keep the max simulation time for later
+    settings['MAX_TIME'] = maxTimeInS
 
     return settings
 
@@ -187,6 +191,19 @@ def setup_cubesats(settings, des_tube_origin, doc):
 
     for ii in range(0, n):
 
+        # angular and linear velocity vectors of Cubesat
+        # [cm/s]
+        v_x = settings['CUBESAT_LIN_VELS']['v_x'][ii]
+        v_y = settings['CUBESAT_LIN_VELS']['v_y'][ii]
+        v_z = settings['CUBESAT_LIN_VELS']['v_z'][ii]
+        v = [v_x, v_y, v_z]
+
+        # [rad/s]
+        omega_x = settings['CUBESAT_ROT_VELS']['omega_x'][ii]
+        omega_y = settings['CUBESAT_ROT_VELS']['omega_y'][ii]
+        omega_z = settings['CUBESAT_ROT_VELS']['omega_z'][ii]
+        omega = [omega_x, omega_y, omega_z]
+
         # get a number (e.g. 1, 2, 3, etc.) corresponding to a size of ('1U',
         # '2U', '3U', etc.)
         # [U]
@@ -198,7 +215,7 @@ def setup_cubesats(settings, des_tube_origin, doc):
 
         # get length of the CubeSat Model
         # [cm]
-        cubesat_length = CubeSatSize * u_size
+        cubesat_length = CubeSatSize * u_size + 2 * cubeSat_tab_size
 
         c4d.documents.MergeDocument(doc, fullFile, 1)
         CubeSatObj = doc.GetActiveObject()
@@ -207,19 +224,24 @@ def setup_cubesats(settings, des_tube_origin, doc):
         # Position the z-location of the CubeSat's origin (position in the
         # length of the tube)
         # [cm]
-        pos[2] = (curr_back_wall + cubesat_length / 2 +
-                  2 * cubeSat_tab_size)
-
-        # actually move the CubeSat
-        pos_rot_obj(CubeSatObj, pos, rot)
+        pos[2] = curr_back_wall + cubesat_length / 2
 
         # update the separation between the current and next cubesat
         if ii < n - 1:
             currSep = settings['CUBESAT_SEPARATIONS'][ii]
-        print currSep
+
         # Move the "back wall" to be the front of the previous cubesat, plus
         # any separation bewtween each cubesat
-        curr_back_wall += cubesat_length + 2 * cubeSat_tab_size + currSep
+        curr_back_wall += cubesat_length + currSep
+
+        # need to define all animation inputs
+        posStart = pos[:]
+
+        rotStart = rot
+
+        animateCubeSat(CubeSatObj, posStart, rotStart,
+                       v, omega, cubesat_length,
+                       settings, des_tube_origin, doc)
 
     return objs
 
@@ -243,6 +265,185 @@ def pos_rot_obj(obj, pos, rot):
     obj()[c4d.ID_BASEOBJECT_REL_POSITION, c4d.VECTOR_X] = pos[0]
     obj()[c4d.ID_BASEOBJECT_REL_POSITION, c4d.VECTOR_Y] = pos[1]
     obj()[c4d.ID_BASEOBJECT_REL_POSITION, c4d.VECTOR_Z] = pos[2]
+
+
+def animateCubeSat(CubeSatObj, posStart, rotStart,
+                   cubeSatLinVel, cubeSatRotVel, cubesat_length, settings,
+                   des_tube_origin, doc):
+
+    # [cm]
+    L = settings['L']
+
+    # maximum distance all of the CubeSats should end up at
+    # [cm]
+    max_dist = settings['MAX_RANGE']
+
+    # time until the cubesats reach MAX_DISTANCE
+    # [s]
+    max_time = settings['MAX_TIME']
+
+    fps = doc.GetFps()
+
+    # getting the starting and ending frames for all of the animations
+    (linVelZStartFrame,
+     rotAndLinXYVelStartFrame,
+     endFrame) = getAnimationFrameNumbers(L, cubesat_length,
+                                          posStart, max_dist,
+                                          des_tube_origin,
+                                          fps, cubeSatLinVel[2])
+
+    # get the ending position and velocity of the CubeSats
+    posEnd, rotEnd = getLastPosAndRot(max_time, fps, cubeSatLinVel,
+                                      cubeSatRotVel, L, posStart,
+                                      rotAndLinXYVelStartFrame, endFrame)
+
+    # get animation tracks for position and rotation
+    obj_type_id = c4d.ID_BASEOBJECT_POSITION
+    xtrack = getXYZtrack(CubeSatObj, obj_type_id, c4d.VECTOR_X, doc)
+    ytrack = getXYZtrack(CubeSatObj, obj_type_id, c4d.VECTOR_Y, doc)
+    ztrack = getXYZtrack(CubeSatObj, obj_type_id, c4d.VECTOR_Z, doc)
+
+    obj_type_id = c4d.ID_BASEOBJECT_ROTATION
+    wxtrack = getXYZtrack(CubeSatObj, obj_type_id, c4d.VECTOR_X, doc)
+    wytrack = getXYZtrack(CubeSatObj, obj_type_id, c4d.VECTOR_Y, doc)
+    wztrack = getXYZtrack(CubeSatObj, obj_type_id, c4d.VECTOR_Z, doc)
+
+    # start recording addition of animations to timeline
+    CubeSatObj.InsertTrackSorted(xtrack)
+    CubeSatObj.InsertTrackSorted(ytrack)
+    CubeSatObj.InsertTrackSorted(ztrack)
+
+    # add position animation - linear variation of position from start to end
+    # frames
+    addValueAtFrame(xtrack, ytrack, ztrack, rotAndLinXYVelStartFrame,
+                    linVelZStartFrame, fps, posStart)
+    addValueAtFrame(xtrack, ytrack, ztrack, endFrame, endFrame, fps, posEnd)
+
+    # add roatation animation - linear variation of rotation from start to end
+    # frames
+    addValueAtFrame(wxtrack, wytrack, wztrack, rotAndLinXYVelStartFrame,
+                    rotAndLinXYVelStartFrame, fps, rotStart)
+    addValueAtFrame(wxtrack, wytrack, wztrack, endFrame, endFrame, fps, rotEnd)
+
+    # Goto Start
+    c4d.CallCommand(12501)
+
+
+def getXYZtrack(obj, trackID, channelID, doc):
+
+    # Find the Track
+    param = c4d.DescID(c4d.DescLevel(trackID, c4d.DTYPE_VECTOR, 0),
+                       c4d.DescLevel(channelID, c4d.DTYPE_REAL, 0)
+                       )
+    track = obj.FindCTrack(param)
+
+    # Create if no track found
+    if not track:
+        track = c4d.CTrack(obj, param)
+        doc.AddUndo(c4d.UNDOTYPE_NEW, track)
+        obj.InsertTrackSorted(track)
+
+    return track
+
+
+def addValueAtFrame(xtrack, ytrack, ztrack, XYStartFrame,
+                    ZStartFrame, fps, startVec):
+
+    # Add a key to each CubeSat linear position tracks
+    xtrack = xtrack.GetCurve()
+    xkey = xtrack.AddKey(c4d.BaseTime(XYStartFrame, fps))['key']
+    xkey.SetValue(xtrack, startVec[0])
+
+    # set the track position variation to linear
+    c4d.CallCommand(465001092)
+
+    ytrack = ytrack.GetCurve()
+    ykey = ytrack.AddKey(c4d.BaseTime(XYStartFrame, fps))['key']
+    ykey.SetValue(ytrack, startVec[1])
+
+    # set the track position variation to linear
+    c4d.CallCommand(465001092)
+
+    ztrack = ztrack.GetCurve()
+    zkey = ztrack.AddKey(c4d.BaseTime(ZStartFrame, fps))['key']
+    zkey.SetValue(ztrack, startVec[2])
+
+    # set the track position variation to linear
+    c4d.CallCommand(465001092)
+
+
+def getLastPosAndRot(max_time, fps, v, omega, L, posStart,
+                     rotAndLinXYVelStartFrame, endFrame):
+
+    # the cubesat can only start rotating / moving in X-Y after its not
+    # constrained by the deployer tube
+    rot_linXY_time = (endFrame - rotAndLinXYVelStartFrame) / fps
+
+    # get the final position of the cubeSat
+    # [cm]
+    x_f = posStart[0] + v[0] * rot_linXY_time
+    y_f = posStart[1] + v[1] * rot_linXY_time
+    z_f = posStart[2] + v[2] * max_time + L
+
+    posEnd = [x_f, y_f, z_f]
+
+    # get the final rotation of the cubeSat
+    rot_linXY_time = (endFrame - rotAndLinXYVelStartFrame) / fps
+
+    # [rad]
+    rotEnd = [rot_linXY_time * omega[0],
+              rot_linXY_time * omega[1],
+              rot_linXY_time * omega[2]]
+
+    return posEnd, rotEnd
+
+
+#
+# @brief      Gets the animation start and stop frame numbers given simulation
+#             parameters
+#
+# @param      L               Length of the Deployer
+#                             [cm]
+# @param      cubesat_length  The cubesat length
+#                             [cm]
+# @param      posStart        The starting centroid position of the cubesat
+#                             [cm]
+# @param      posEnd          The ending centroid position of the cubesat
+#                             [cm]
+# @param      fps             The fps of the simulation
+# @param      v_z             The main cubesat velocity component
+#
+# @return     [start frame for lin. z velocity animation,
+#              start frame for rot. and lin. x-y velocity animation,
+#              start frame for both lin. & rot velocity animation]
+#
+def getAnimationFrameNumbers(L, cubesat_length, posStart, maxDist,
+                             des_tube_origin, fps, v_z):
+
+    # need to compute frame numbers for where to apply the animation starts and
+    # stops for linear and rotational animations
+
+    # we need the CubeSats to be moving linearly in z from the very first frame
+    linVelZStartFrame = 0
+
+    # defines the initial location of the CubeSat's centroid
+    # [cm]
+    d_o = posStart[2]
+
+    # defines the exact distance of the CubeSat's centroid when it has cleared
+    # the deployer
+    # [cm]
+    d_f = (cubesat_length / 2) + des_tube_origin[2] + L
+
+    # the CubeSats can only start rotating once they are clear from the
+    # deployer
+    delta_z = d_f - d_o
+    rotAndLinXYVelStartFrame = (delta_z / v_z) * fps
+
+    # last frame is the same for both linear and rotational motion
+    endFrame = linVelZStartFrame + ((L + maxDist) / v_z) * fps
+
+    return linVelZStartFrame, rotAndLinXYVelStartFrame, endFrame
 
 
 if __name__ == '__main__':
