c014f892e613319437acfb790655a4012730705a
Nicholas Renninger <nire1188@colorado.edu>
2019-02-18 13:44:16 -0700

    Major updates to the simulation setup with new deployer models, adding ability to easily move VANTAGE tube number

--- runSimulation_main.py
+++ runSimulation_main.py
@@ -3,17 +3,18 @@ import yaml
 import os
 
 __author__ = "Nicholas Renninger, Jerry Wang"
-__copyright__ = "'Copyright' 2018, VANTAGE"
-__credits__ = ["Long Dong Silver"]
+__copyright__ = "'Copyright' 2019, VANTAGE"
+__credits__ = ["Long Song Silver"]
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Nicholas Renninger"
 __email__ = "nicholas.renninger@colorado.edu"
 __status__ = "Development"
 
 
 def main():
-    settingsFile = os.path.join('config', 'config_simulation_template.yaml')
+    settingsFile = os.path.join('config',
+                                'config_simulation_template.yaml')
 
     runAndSaveSimulation(settingsFile)
 
@@ -27,6 +28,7 @@ def main():
 # @return     a saved C4D simulation case in the C4D case simulation directory
 #
 def runAndSaveSimulation(settingsFile):
+
     # this is the current c4d session we need to modify
     doc = c4d.documents.GetActiveDocument()
 
@@ -41,7 +43,7 @@ def runAndSaveSimulation(settingsFile):
     setup_cubesats(settings, des_tube_origin, doc)
 
     # add the camera after the VANTAGE origin has already been set
-    addcamera(settings, doc)
+    # addcamera(settings, doc)
 
     print 'Done with loading in simulation case. Have Fun!!'
 
@@ -59,6 +61,13 @@ def runAndSaveSimulation(settingsFile):
 # @return     Settings dict from YAML config
 #
 def simulationSetup(settingsFile, doc):
+
+    # clear all simulation objects
+    # Select All
+    c4d.CallCommand(100004766)
+    # Delete
+    c4d.CallCommand(100004787)
+
     # Read YAML settings file
     with open(settingsFile, 'r') as stream:
         settings = yaml.load(stream)
@@ -163,55 +172,114 @@ def addcamera(settings, doc):
 #                  [x, y, z] ~ [cm]
 #
 def setup_deployer(settings, doc):
+
     dep = settings['DEPLOYER_FILEPATH']
     gDrive = settings['G_DRIVE_PATH']
     fullFile = os.path.join(gDrive, dep)
 
-    # Import Deployer STL
+    # Import Deployer model
     c4d.documents.MergeDocument(doc, fullFile, 1)
     deployerObj = doc.GetActiveObject()
 
     # Position and Rotate the Deployer so the global origin is at VANTAGE's
     # origin
-    VANTAGE_origin_pos = settings['VANTAGE_POS']
+    #
+    # After moving the deployer origin
+    # to the correct position in the global coordinate frame, then move the
+    # deployer origin from the centerline to the VCF origin according to these
+    # parameters
+    #
+    # This location is just a tube number (1-6), so need to use geometry
+    # defined in 13-5 to turn into global coords
+    VANTAGE_tube_num = settings['VANTAGE_POS']
+    launch_tube_num = settings['DESIRED_DEPLOYMENT_TUBE']
+
+    if launch_tube_num == VANTAGE_tube_num:
+        msg = 'You can''t launch (tube ' +\
+              str(launch_tube_num) +\
+              ') from the same tube as VANTAGE (tube ' +\
+              str(VANTAGE_tube_num) + ')'
+        raise ValueError(msg)
+
+    rho = settings['RHO']
+    nabla = settings['NABLA']
+
+    # convert focal length from [mm] to [cm]
+    f = settings['FOCAL_LENGTH'] / 10.
+    p_optical = settings['P_OPTICAL']
+
+    # need to figure out where the VCF origin is relative to the deployer
+    # origin
+    # [cm]
+    d_optical = p_optical - f
+    z_loc_of_VCF = d_optical
+
+    desired_tube = VANTAGE_tube_num
+    translVec = [nabla, rho, z_loc_of_VCF]
+    VANTAGE_origin_pos = calculateOriginFromTubeNumber(desired_tube,
+                                                       settings, translVec)
+
+    # solidworks models have x and y axes flipped, so correct this
+    VANTAGE_origin_pos[0] *= -1
+    VANTAGE_origin_pos[1] *= -1
     VANTAGE_origin_rot = settings['VANTAGE_ROT']
     pos_rot_obj(deployerObj, VANTAGE_origin_pos, VANTAGE_origin_rot)
 
     c4d.EventAdd()
 
-    # save deployer geometry variables
-    mu = settings['MU']
-    beta = settings['BETA']
-    sigma = settings['SIGMA']
-    psi = settings['PSI']
-    gamma = settings['GAMMA']
-    L = settings['L']
+    # define the deployment tube origin
 
-    # define the origin of the desired tube relative to the deployer origin
-    desired_tube = settings['DESIRED_DEPLOYMENT_TUBE']
-    tube_coeffs = settings['TUBE_ORIGIN_COEFFS'][desired_tube - 1]
-    x_coeff = tube_coeffs['x']
-    y_coeff = tube_coeffs['y']
-    z_coeff = tube_coeffs['z']
-
-    des_tube_x = x_coeff[0] * sigma + x_coeff[1] * psi + x_coeff[2] * gamma
-    des_tube_y = y_coeff[0] * mu + y_coeff[1] * beta
-    des_tube_z = z_coeff[0] * L
-
-    # now, translate this tube origin to its correct place in the global
-    # coordinate system by adding the translation that moves the deployer
-    # origin to the global origin
-    des_tube_x += VANTAGE_origin_pos[0]
-    des_tube_y += VANTAGE_origin_pos[1]
-    des_tube_z += VANTAGE_origin_pos[2]
-
-    # package output
-    des_tube_origin = [des_tube_x, des_tube_y, des_tube_z]
+    # need to locate tube origin in global coords by first calculating the
+    # relative tube origin then adding to this relative position the location
+    # of the deployer's origin in global coords
+    # [cm]
+    translVec = VANTAGE_origin_pos
+    desired_tube = launch_tube_num
+    des_tube_origin = calculateOriginFromTubeNumber(desired_tube,
+                                                    settings, translVec)
 
     # Kobe
     print "Loaded Deployer Model"
 
     return deployerObj, des_tube_origin
+
+
+#
+# @brief      Calculates the tube origin given the desired tube and the
+#
+# @param      desired_tube  The desired tube
+# @param      settings      Settings dict from YAML config
+# @param      translVec     The translation vector to move the tube origin to
+#                           the origin of the final desired frame
+#
+# @return     The tube origin vector in global coords [cm]
+#
+def calculateOriginFromTubeNumber(desired_tube, settings, translVec):
+
+    # save deployer geometry variables
+    mu = settings['MU']
+    psi = settings['PSI']
+    gamma = settings['GAMMA']
+    L = settings['L']
+
+    tube_coeffs = settings['TUBE_ORIGIN_COEFFS'][desired_tube - 1]
+    x_coeff = tube_coeffs['x']
+    y_coeff = tube_coeffs['y']
+    z_coeff = tube_coeffs['z']
+
+    des_tube_x = (x_coeff[0] * psi + 0.5 * x_coeff[1] * gamma)
+    des_tube_y = y_coeff[0] * mu
+    des_tube_z = (0.5 * L) * z_coeff[0]
+
+    # now, translate this tube origin to its correct place in the global
+    # coordinate system by adding the translation that moves the tube origin by
+    # the desired final translation vector
+    des_tube_x += translVec[0]
+    des_tube_y += translVec[1]
+    des_tube_z += translVec[2]
+
+    # package output
+    return [des_tube_x, des_tube_y, des_tube_z]
 
 
 #
@@ -312,6 +380,8 @@ def setup_cubesats(settings, des_tube_origin, doc):
         # need to define all animation inputs
         posStart = pos[:]
         rotStart = rot[:]
+
+        print(posStart)
 
         # move cubesat to the right location and animate it's motion
         # (translation and rotation) over the duration of the simulation
