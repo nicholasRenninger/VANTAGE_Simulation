e8d26007d468482e6a28d774e9215a19f62979c3
Nicholas Renninger <nire1188@colorado.edu>
2019-04-16 03:40:31 -0600

    added code to clean out junk from simulation runs after they're all run.

---
+++ cleanCaseToFDirs.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+__author__ = "Nicholas Renninger"
+__copyright__ = "'Copyright' 2019, VANTAGE"
+__credits__ = ["Long Song Silver"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Nicholas Renninger"
+__email__ = "nicholas.renninger@colorado.edu"
+__status__ = "Development"
+
+
+#
+# @brief      removes all non-noisy pcd output files
+#
+# @param      outputDir  The output directory to clean
+#
+# @return     outputDir now only contains noisy output pcd files
+#
+def cleanOutputDir(outputDir):
+
+    # black magic and satanic worship used below
+    r = re.compile(r'^((?!.*simArchive).).((?!.*noisy).)*(pcd)$')
+
+    # 3AM hacker black magic
+    pathsToUnwantedFiles = [os.path.join(dirpath, f)
+                            for dirpath, dirnames, files in os.walk(outputDir)
+                            for f in list(filter(r.match, files))]
+
+    for file in pathsToUnwantedFiles:
+        os.remove(file)
+
+
+if __name__ == '__main__':
+
+    outputDir = 'D:\\Google Drive\\Undergrad\\VANTAGE\\13 Simulation\\4_Simulation_Cases'
+
+    cleanOutputDir(outputDir)
